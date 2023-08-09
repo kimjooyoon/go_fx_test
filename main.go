670d5637d0be8e6b0c46340eebf2b4c4c3f0bcfb
kimjooyoon <asketeddy@gmail.com>
2023-08-09 22:47:47 +0900

    add ::NewHttpServer

--- main.go
+++ main.go
@@ -1,8 +1,39 @@
 package main
 
-import "go.uber.org/fx"
+import (
+	"context"
+	"go.uber.org/fx"
+	"net"
+	"net/http"
+)
 
 func main() {
-	fx.New().Run()
-	
+	fx.New(
+		fx.Provide(NewHttpServer),
+	).Run()
+}
+
+func NewHttpServer(lc fx.Lifecycle) *http.Server {
+	srv := &http.Server{Addr: ":8080"}
+
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			ln, err := net.Listen("tcp", srv.Addr)
+			if err != nil {
+				return err
+			}
+			go func() {
+				err := srv.Serve(ln)
+				if err != nil {
+					return
+				}
+			}()
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			return srv.Shutdown(ctx)
+		},
+	})
+
+	return srv
 }
