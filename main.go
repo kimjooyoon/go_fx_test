0e4b9a0d2a4c72f856e9fec113457c2d8adab279
kimjooyoon <asketeddy@gmail.com>
2023-08-09 23:19:14 +0900

    add handler

--- main.go
+++ main.go
@@ -1,40 +1,17 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"go.uber.org/fx"
-	"net"
 	"net/http"
 )
 
 func main() {
 	fx.New(
-		fx.Provide(NewHTTPServer),
+		fx.Provide(
+			NewHTTPServer,
+			NewServeMux,
+			NewEchoHandler,
+		),
 		fx.Invoke(func(*http.Server) {}),
 	).Run()
-}
-
-func NewHTTPServer(lc fx.Lifecycle) *http.Server {
-	srv := &http.Server{Addr: ":8080"}
-	lc.Append(fx.Hook{
-		OnStart: func(ctx context.Context) error {
-			ln, err := net.Listen("tcp", srv.Addr)
-			if err != nil {
-				return err
-			}
-			fmt.Println("Starting HTTP server at", srv.Addr)
-			go func() {
-				err := srv.Serve(ln)
-				if err != nil {
-					return
-				}
-			}()
-			return nil
-		},
-		OnStop: func(ctx context.Context) error {
-			return srv.Shutdown(ctx)
-		},
-	})
-	return srv
 }
---
+++ server.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"go.uber.org/fx"
+	"net"
+	"net/http"
+)
+
+func NewHTTPServer(lc fx.Lifecycle, mux *http.ServeMux) *http.Server {
+	srv := &http.Server{Addr: ":8080", Handler: mux}
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			ln, err := net.Listen("tcp", srv.Addr)
+			if err != nil {
+				return err
+			}
+			fmt.Println("Starting HTTP server at", srv.Addr)
+			go func() {
+				err := srv.Serve(ln)
+				if err != nil {
+					return
+				}
+			}()
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			return srv.Shutdown(ctx)
+		},
+	})
+	return srv
+}
